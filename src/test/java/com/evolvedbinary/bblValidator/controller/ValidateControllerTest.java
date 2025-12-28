4d03815bc2732760f26235cd727f12cd8fcf89a9
marmoure <tinky93winky@gmail.com>
2025-12-28 04:12:38 +0100

    [test] implement missing tests

--- src/test/java/com/evolvedbinary/bblValidator/controller/ValidateControllerTest.java
+++ src/test/java/com/evolvedbinary/bblValidator/controller/ValidateControllerTest.java
@@ -1,6 +1,7 @@
 package com.evolvedbinary.bblValidator.controller;
 
 import com.evolvedbinary.bblValidator.dto.ErrorResponse;
+import com.evolvedbinary.bblValidator.dto.ValidationForm;
 import com.evolvedbinary.bblValidator.dto.ValidationResponse;
 import io.micronaut.context.annotation.Value;
 import io.micronaut.http.HttpRequest;
@@ -17,6 +18,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 import java.util.Optional;
 
 import static com.evolvedbinary.bblValidator.filter.ApiVersionFilter.BBLVALIDATOR_VERSION_HEADER;
@@ -146,32 +148,189 @@ public class ValidateControllerTest {
 
     @Test
     void provideUrlAndValidateCsvFromForm() {
-        fail("TODO implement");
+        String url = "https://raw.githubusercontent.com/marmoure/bbl-validator/refs/heads/feature/vlidation/src/test/resources/schemas/concatPass.csv";
+        String schemaId = "concat";
+        Map<String, String> formBody = Map.of(
+                "schemaId", schemaId,
+                "url", url
+        );
+
+        HttpRequest<?> request = HttpRequest.POST("/", formBody)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpResponse<ValidationResponse> response = client.toBlocking().exchange(request, ValidationResponse.class);
+
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals(Optional.of(MediaType.APPLICATION_JSON_TYPE), response.getContentType());
+        assertEquals(version, response.getHeaders().get(BBLVALIDATOR_VERSION_HEADER));
+
+        assertTrue(response.getBody().isPresent());
+
+        ValidationResponse validationResponse = response.getBody().get();
+
+        assertTrue(validationResponse.isValid());
+        assertTrue(validationResponse.getErrors().isEmpty());
+        assertTrue(validationResponse.getExecutionTimeMs() > -1);
+
     }
 
     @Test
     void provideUrlAndValidateInvalidCsvFromForm() {
-        fail("TODO implement");
+        String url = "https://raw.githubusercontent.com/marmoure/bbl-validator/refs/heads/feature/vlidation/src/test/resources/schemas/concatFail.csv";
+        String schemaId = "concat";
+        Map<String, String> formBody = Map.of(
+                "schemaId", schemaId,
+                "url", url
+        );
+
+        HttpRequest<?> request = HttpRequest.POST("/", formBody)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpResponse<ValidationResponse> response = client.toBlocking().exchange(request, ValidationResponse.class);
+
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals(Optional.of(MediaType.APPLICATION_JSON_TYPE), response.getContentType());
+        assertEquals(version, response.getHeaders().get(BBLVALIDATOR_VERSION_HEADER));
+
+        assertTrue(response.getBody().isPresent());
+
+        ValidationResponse validationResponse = response.getBody().get();
+        
+        assertFalse(validationResponse.isValid());
+        assertFalse(validationResponse.getErrors().isEmpty());
+
+        validationResponse.getErrors().forEach(error -> {
+            assertNotNull(error.getMessage());
+            assertFalse(error.getMessage().isEmpty());
+            assertTrue(error.getLineNumber() > 0);
+            assertTrue(error.getColumnIndex() >= 0);
+        });
+        
+        String errorMessage = "is(concat($c1, $c2)) fails for row: 3, column: c3, value: \"ccccc\"";
+
+        assertEquals(errorMessage, validationResponse.getErrors().getFirst().getMessage());
+        assertEquals(3, validationResponse.getErrors().getFirst().getLineNumber());
+        assertEquals(3, validationResponse.getErrors().getFirst().getColumnIndex());
+        
+        assertTrue(validationResponse.getExecutionTimeMs() > -1);
+        
     }
 
     @Test
     void provideUrlAndValidateCsvInQueryString() {
-        fail("TODO implement");
+        String url = "https://raw.githubusercontent.com/marmoure/bbl-validator/refs/heads/feature/vlidation/src/test/resources/schemas/concatPass.csv";
+        String schemaId = "concat";
+
+        HttpRequest<Void> request = HttpRequest.POST("/?schema-id=" + schemaId + "&url=" + url, null);
+
+        HttpResponse<ValidationResponse> response = client.toBlocking().exchange(request, ValidationResponse.class);
+
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals(Optional.of(MediaType.APPLICATION_JSON_TYPE), response.getContentType());
+        assertEquals(version, response.getHeaders().get(BBLVALIDATOR_VERSION_HEADER));
+
+        assertTrue(response.getBody().isPresent());
+
+        ValidationResponse validationResponse = response.getBody().get();
+
+        assertTrue(validationResponse.isValid());
+        assertTrue(validationResponse.getErrors().isEmpty());
+        assertTrue(validationResponse.getExecutionTimeMs() > -1);
     }
 
     @Test
     void provideUrlAndValidateInvalidCsvInQueryString() {
-        fail("TODO implement");
+        String url = "https://raw.githubusercontent.com/marmoure/bbl-validator/refs/heads/feature/vlidation/src/test/resources/schemas/concatFail.csv";
+        String schemaId = "concat";
+
+        HttpRequest<Void> request = HttpRequest.POST("/?schema-id=" + schemaId + "&url=" + url, null);
+
+        HttpResponse<ValidationResponse> response = client.toBlocking().exchange(request, ValidationResponse.class);
+        
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals(Optional.of(MediaType.APPLICATION_JSON_TYPE), response.getContentType());
+        
+        assertTrue(response.getBody().isPresent());
+
+        ValidationResponse validationResponse = response.getBody().get();
+        
+        assertFalse(validationResponse.isValid());
+        assertFalse(validationResponse.getErrors().isEmpty());
+
+        validationResponse.getErrors().forEach(error -> {
+            assertNotNull(error.getMessage());
+            assertFalse(error.getMessage().isEmpty());
+            assertTrue(error.getLineNumber() > 0);
+            assertTrue(error.getColumnIndex() >= 0);
+        });
+        
+        String errorMessage = "is(concat($c1, $c2)) fails for row: 3, column: c3, value: \"ccccc\"";
+
+        assertEquals(errorMessage, validationResponse.getErrors().getFirst().getMessage());
+        assertEquals(3, validationResponse.getErrors().getFirst().getLineNumber());
+        assertEquals(3, validationResponse.getErrors().getFirst().getColumnIndex());
+        
+        assertTrue(validationResponse.getExecutionTimeMs() > -1);
+
     }
 
     @Test
     void provideNonResolvableUrlAndValidateCsvFromForm() {
-        fail("TODO implement");
+        String url = "nothing";
+        String schemaId = "concat";
+        Map<String, String> formBody = Map.of(
+                "schemaId", schemaId,
+                "url", url
+        );
+
+        HttpRequest<?> request = HttpRequest.POST("/", formBody)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpClientResponseException exception = assertThrows(HttpClientResponseException.class, () -> client.toBlocking().exchange(request, String.class));
+
+        // assert the response status
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
+
+        // assert the X-BBLVALIDATOR-VERSION is present and it's returning the expected value.
+        assertEquals(version, exception.getResponse().getHeaders().get(BBLVALIDATOR_VERSION_HEADER));
+
+        // assert the response type
+        assertEquals(Optional.of(MediaType.APPLICATION_JSON_TYPE), exception.getResponse().getContentType());
+
+        // Get the error response
+        ErrorResponse errorBody = exception.getResponse().getBody(ErrorResponse.class).orElse(null);
+        assertNotNull(errorBody);
+        assertEquals(ErrorResponse.Code.NON_RESOLVABLE_URL, errorBody.getCode());
+        assertEquals("Unable to resolve url : nothing", errorBody.getDescription());
     }
 
     @Test
     void provideNonCsvUrlAndValidateCsvFromForm() {
-        fail("TODO implement");
+        String url = "https://picsum.photos/200/300";
+        String schemaId = "concat";
+        Map<String, String> formBody = Map.of(
+                "schemaId", schemaId,
+                "url", url
+        );
+
+        HttpRequest<?> request = HttpRequest.POST("/", formBody)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpResponse<ValidationResponse> response = client.toBlocking().exchange(request, ValidationResponse.class);
+
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals(Optional.of(MediaType.APPLICATION_JSON_TYPE), response.getContentType());
+        assertEquals(version, response.getHeaders().get(BBLVALIDATOR_VERSION_HEADER));
+
+        assertTrue(response.getBody().isPresent());
+
+        ValidationResponse validationResponse = response.getBody().get();
+        
+        assertFalse(validationResponse.isValid());
+        assertFalse(validationResponse.getErrors().isEmpty());
+
+        assertTrue(validationResponse.getExecutionTimeMs() > -1);
+
     }
 
     @Test
